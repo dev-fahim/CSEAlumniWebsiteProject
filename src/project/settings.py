71667d96dbebd5f65d3a4af4ac4b897d7b749b78
Fahim Al Islam <fahim6668@gmail.com>
2020-02-07 21:13:24 +0600

    added

--- src/project/settings.py
+++ src/project/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/cse/static/'
+STATIC_URL = '/static/'
 
-MEDIA_URL = '/cse/media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
